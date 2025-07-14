d18ca336a843e4e6e2626ebf7f856ce9595b3a6b
Sahil Saini <155174291+sahil16th@users.noreply.github.com>
2025-07-15 02:03:17 +0530

    Update db.js

--- backend/MongoDB/db.js
+++ backend/MongoDB/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect('mongodb+srv://Sahil16_th:OgQHk4kTytV35FjR@cluster0.ds5mamr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/mydatabase');
+        await mongoose.connect('mongodb+srv://sahilsaini22001:sahilsaini22001@cluster0.en552d2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/mydatabase');
         console.log("✔️  Connected to DataBase");
     } catch (error) {
         console.log("❌  Failed to connect DataBase");
