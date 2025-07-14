0209e059db8e3e7e61a83d0748a62973792eae44
Sahil Saini <155174291+sahil16th@users.noreply.github.com>
2025-07-15 01:42:17 +0530

    Update db.js

--- backend/MongoDB/db.js
+++ backend/MongoDB/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/mydatabase');
+        await mongoose.connect('mongodb+srv://Sahil16_th:OgQHk4kTytV35FjR@cluster0.ds5mamr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/mydatabase');
         console.log("✔️  Connected to DataBase");
     } catch (error) {
         console.log("❌  Failed to connect DataBase");
